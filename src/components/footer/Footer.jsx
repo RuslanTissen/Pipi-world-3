8ce0aba313f61681739ae871c388c9a542c6ab28
SFERAS <rthyssen73@gmail.com>
2024-03-02 10:25:38 +0100

    footer updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
-import { FaWhatsapp, FaInstagram} from 'react-icons/fa'
+import { FaWhatsapp, FaInstagram } from 'react-icons/fa'
 import { SiGmail, SiUpwork } from "react-icons/si";
 import { IoDiamondSharp } from "react-icons/io5";
 // import trans from '../Translate.js';
@@ -20,16 +20,39 @@ function Footer() {
 
 	return (
 		<div className='footer'>
-			<div className="footer__operation footer__operation--block">
+
+			<div className="footer__logo footer__operation--block">
+				<h2 className='footer__logo--logo-box' ><NavLink to="*" onClick={scrollToTheTop}><span>PIPI` <IoDiamondSharp /></span>SPORTS  <span />PHILOSOPHY <span /></NavLink></h2>
+
+				<p>Copyright © 2024 by <a href="https://tissen.tech/" target='_blank'>Tissen Studio</a> Powered</p>
+
+			</div>
+		</div>
+	)
+}
+
+export default Footer
+
+
+
+// <div className="footer__logo--social-container">
+// 	<NavLink className='footer__logo--social-container-link' to="Contact" onClick={scrollToTheTop}><SiGmail /></NavLink>
+// 	<a className='footer__logo--link-whatsapp' href='https://wa.me/14155797059' target='_blank'><FaWhatsapp /></a>
+// 	<a className='footer__logo--social-container-link' href='https://www.upwork.com/freelancers/~01fe4cfc87ce82d6a8' target='_blank'><SiUpwork /></a>
+// 	<a className='footer__logo--social-container-link' href='https://www.instagram.com/pipitosti/' target='_blank'><FaInstagram /></a>
+// </div>
+
+
+{/* <div className="footer__operation footer__operation--block">
 				<h3>HOURS OF OPERATION </h3>
 
 				<p>Mon-Thu: 9AM to 8PM</p>
 				<p>Friday: 9AM to 3PM</p>
 				<p>Sat-Sun: Closed</p>
 
-			</div>
+			</div> */}
 
-			<div className="footer__contact footer__operation--block">
+{/* <div className="footer__contact footer__operation--block">
 				<h3>CONTACT ME</h3>
 
 				<p>500 Terry Francine Street</p>
@@ -38,9 +61,9 @@ function Footer() {
 				<p>Mail: info@mysite.com</p>
 				<p>Tel: 123-456-7890</p>
 
-			</div>
-			
-			<div className="footer__menu footer__operation--block">
+			</div> */}
+
+{/* <div className="footer__menu footer__operation--block">
 				<h3>MENU</h3>
 
 				<p><IoDiamondSharp /> About</p>
@@ -50,22 +73,4 @@ function Footer() {
 				<p><IoDiamondSharp /> Contact</p>
 
 				<NavLink className="footer__menu--link-to-contact" to="Contact" onClick={scrollToTheTop}>{trans[lang].footerContact}</NavLink>
-			</div>
-
-			<div className="footer__logo footer__operation--block">
-				<h2 className='footer__logo--logo-box' ><NavLink to="*" onClick={scrollToTheTop}><span>PIPI` <IoDiamondSharp /></span>SPORTS  <span />PHILOSOPHY <span /></NavLink></h2>
-
-				<div className="footer__logo--social-container">
-					<NavLink className='footer__logo--social-container-link' to="Contact" onClick={scrollToTheTop}><SiGmail /></NavLink>
-					<a className='footer__logo--link-whatsapp' href='https://wa.me/14155797059' target='_blank'><FaWhatsapp /></a>
-					<a className='footer__logo--social-container-link' href='https://www.upwork.com/freelancers/~01fe4cfc87ce82d6a8' target='_blank'><SiUpwork /></a>
-					<a className='footer__logo--social-container-link' href='https://www.instagram.com/pipitosti/' target='_blank'><FaInstagram /></a>
-				</div>
-
-				<p>© 2024 by TISSEN  Powered</p>
-			</div>
-		</div>
-	)
-}
-
-export default Footer
+			</div> */}
